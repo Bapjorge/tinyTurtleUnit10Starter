3fddb4d843fe0058610760193f121dff511cdcb7
Jorge Moreno <jorge.a.moreno.personal@gmail.com>
2017-04-03 22:46:37 +0000

    abstract

--- script.js
+++ script.js
@@ -1,23 +1,55 @@
 TinyTurtle.apply(window);
-stamp();
 
-function shape(len,ang){
-    forward(len);
-    right(ang);
-      forward(len);
-    right(ang);
-      forward(len);
-    right(ang);
-       forward(len);
-    right(ang);
-      forward(len);
-    right(ang);
-      forward(len);
-    right(ang);
-
+function shape(){
+  for(var i = 0; i<4000; i=i+1){
+  forward(25);
+  right(40);
+  forward(10);
+  right(30);
+  }
 }
 
 // Type your function call below
 
-shape(60,90 );
+shape();
+
+function shapee(){
+  for(var i = 0; i<40; i=i+1){
+  forward(15);
+  right(8);
+  forward(10);
+  right(30);
+  }
+}
+
+// Type your function call below
+
+shapee();
+
+function shapeee(){
+  for(var i = 0; i<20; i=i+1){
+  forward(10);
+  right(4);
+  forward(5);
+  right(15);
+  }
+}
+
+// Type your function call below
+
+shapeee();
+
+
+function shapeeee(){
+  for(var i = 0; i<10; i=i+1){
+   forward(40);
+   right(90);
+  
+  }
+}
+for(var i = 0; i<200; i=i+1){
+shapeeee();
+right(30);
+}
+// shapeeee();
 
